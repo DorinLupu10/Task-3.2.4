229594979c9a78c48a2be3db320d9680e706aabe
Dorin Lupu <dorin.lupu@outlook.com>
2026-05-26 19:52:03 +0300

    Set Route table

--- main.tf
+++ main.tf
@@ -17,3 +17,29 @@ resource "aws_subnet" "public" {
     Name = "dorin-public-subnet"
   }
 }
+
+# Gateway
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "dorin-igw"
+  }
+}
+
+# Route Table
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+
+}
+
+# Asociere Route Table cu Subnet
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
+}
