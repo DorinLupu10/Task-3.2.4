d4a56e7e337f413ff086c42969b69f73a8f5b540
Dorin Lupu <dorin.lupu@outlook.com>
2026-06-08 22:36:33 +0300

    add script for start CloudWatch

--- install.sh
+++ install.sh
@@ -188,3 +188,10 @@ cat > /opt/aws/amazon-cloudwatch-agent/etc/amazon-cloudwatch-agent.json <<'EOF'
 }
 EOF
 
+
+# Pornire CloudWatch
+/opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl \
+  -a fetch-config \
+  -m ec2 \
+  -c file:/opt/aws/amazon-cloudwatch-agent/etc/amazon-cloudwatch-agent.json \
+  -s
