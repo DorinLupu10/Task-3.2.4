f1c1d76c9220e01b87a03362ca5f001daa8d966d
Dorin Lupu <dorin.lupu@outlook.com>
2026-06-17 20:39:28 +0300

    Set  Load Balancer

--- main.tf
+++ main.tf
@@ -586,3 +586,47 @@ resource "aws_lb_target_group" "main" {
     Name = "dorin-tg"
   }
 }
+
+# Application Load Balancer
+resource "aws_lb" "main" {
+  name               = "dorin-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb.id]
+  subnets            = module.vpc.public_subnets
+
+  tags = {
+    Name = "dorin-alb"
+  }
+}
+
+# ALB Listener HTTP - redirect to HTTPS
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.main.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type = "redirect"
+
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
+  }
+}
+
+# ALB Listener HTTPS
+resource "aws_lb_listener" "https" {
+  load_balancer_arn = aws_lb.main.arn
+  port              = 443
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-TLS13-1-2-2021-06"
+  certificate_arn   = module.acm_alb.acm_certificate_arn
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.main.arn
+  }
+}
