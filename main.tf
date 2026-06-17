2b152402f8eea1963945988beddff2e50d6ebefe
Dorin Lupu <dorin.lupu@outlook.com>
2026-06-17 15:16:07 +0300

    Set target group

--- main.tf
+++ main.tf
@@ -565,3 +565,24 @@ resource "aws_security_group" "alb" {
     Name = "dorin-alb-sg"
   }
 }
+
+# Target Group
+resource "aws_lb_target_group" "main" {
+  name     = "dorin-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = module.vpc.vpc_id
+
+  health_check {
+    path                = "/api/v1/health"
+    protocol            = "HTTP"
+    healthy_threshold   = 5
+    unhealthy_threshold = 2
+    timeout             = 5
+    interval            = 30
+  }
+
+  tags = {
+    Name = "dorin-tg"
+  }
+}
