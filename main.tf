7f609164e6e9cd6d6ebeb59a801fd25a0a6b0969
Dorin Lupu <dorin.lupu@outlook.com>
2026-06-17 15:15:26 +0300

    Set security group for alb

--- main.tf
+++ main.tf
@@ -531,3 +531,37 @@ module "acm_alb" {
 
   wait_for_validation = true
 }
+
+# Security Group pentru ALB
+resource "aws_security_group" "alb" {
+  name        = "dorin-alb-sg"
+  description = "Security group for ALB"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "dorin-alb-sg"
+  }
+}
